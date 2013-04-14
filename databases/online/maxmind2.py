7b982778acdb8d360fcb325276e6b8411d6b7b04
Darafei Praliaskouski <komzpa@gmail.com>
2013-04-14 16:23:10 +0300

    [online/maxmind] accuracy tweaks

--- databases/online/maxmind2.py
+++ databases/online/maxmind2.py
@@ -6,6 +6,11 @@ def get_location(req = {}):
     ip = req.get("ip", "me")
     try:
         resp = json.loads(urllib.urlopen("http://www.maxmind.com/geoip/city_isp_org/%s?demo=1"%ip).read())
-        return {"position":{"type":"ip", "latitude": resp["location"]["latitude"], "longitude": resp["location"]["longitude"], "accuracy": 130000.}, "service": "maxmind geoip demo"}
+        acc = 140000
+        if (resp["location"]["latitude"]) != int(resp["location"]["latitude"]):
+            acc /= 2
+        if (resp["location"]["longitude"]) != int(resp["location"]["longitude"]):
+            acc /= 2
+        return {"position":{"type":"ip", "latitude": resp["location"]["latitude"], "longitude": resp["location"]["longitude"], "accuracy": acc}, "service": "maxmind geoip demo"}
     except:
         return False
