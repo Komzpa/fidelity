c5df735fba7eef8ef3402583c6aaf73a8dbed2bf
Darafei Praliaskouski <komzpa@mapswith.me>
2013-09-26 13:29:36 +0300

    gps location proxy provider

---
+++ databases/offline/gps.py
@@ -0,0 +1,17 @@
+import time
+
+def get_location(req = {}, timestamp = None):
+    device_time = req.get("gps",req.get("clock",{})).get("currentTime", time.time())
+    if not timestamp:
+        timestamp = time.time()
+    try:
+        if "gps" in req:
+            gps = req["gps"]
+            timedelta = min([
+                abs(timestamp - gps["time"]/1000),
+                abs(device_time - gps["time"]/1000)
+            ])
+            acc = 3 * gps["accuracy"] + ( timedelta * max(gps.get("speed", 6), 6) )
+            return {"position":{"type":"gps", "latitude": gps["latitude"], "longitude": gps["longitude"], "accuracy": acc}, "service": "gps proxy"}
+    except KeyError:
+        pass
