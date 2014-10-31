aa9afdf976f3e2e82678edd3e1efd8ac8f0e44f1
Darafei Praliaskouski <komzpa@gmail.com>
2014-10-31 16:01:58 +0300

    [gps] when retrieving location from logs, use correct timestamp

--- databases/offline/gps.py
+++ databases/offline/gps.py
@@ -1,7 +1,7 @@
 import time
 
 def get_location(req = {}, timestamp = None):
-    device_time = req.get("gps",req.get("clock",{})).get("currentTime", time.time())
+    device_time = req.get("clock",{}).get("currentTime", 0) / 1000
     if not timestamp:
         timestamp = time.time()
     try:
@@ -11,10 +11,12 @@ def get_location(req = {}, timestamp = None):
                 abs(timestamp - gps["time"]/1000),
                 abs(device_time - gps["time"]/1000)
             ])
+            #print timestamp, device_time, gps['time'], timestamp/ gps['time'],device_time/ gps['time'],
             acc = gps["accuracy"] + ( timedelta * max(gps.get("speed", 6), 6) )
             pos = {"type":"gps", "latitude": gps["latitude"], "longitude": gps["longitude"], "accuracy": acc}
             if "altitude" in gps:
                 pos["altitude"] = gps["altitude"]
+            pos["time"] = device_time
             return {"position": pos, "service": "gps proxy"}
     except KeyError:
         pass
