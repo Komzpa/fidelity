3f8a651b81f8b1ffd3216e48b55a453088e7eb3a
Darafei Praliaskouski <komzpa@gmail.com>
2014-11-03 16:16:26 +0300

    [arp] arp sensor for wirefull computers

---
+++ sensors/arp.py
@@ -0,0 +1,18 @@
+import subprocess
+
+# This module is using system ARP table to detect device mac addresses
+
+def get_state():
+    arp_table = open('/proc/net/arp', 'r')
+#    wifi = []
+    macs = set() 
+    arp_table.next()
+    for line in arp_table:
+        line = line.strip().split()
+	macs.add(line[3])
+    macs.discard('00:00:00:00:00:00')
+    wifi = [{'mac':i} for i in macs]
+    return {"wifi": wifi}
+
+if __name__ == "__main__":
+    print get_state()
